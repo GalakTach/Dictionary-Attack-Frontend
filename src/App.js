8bfb4290e7eb780aa7593a559486c0f0c1c9647a
Angelo Leonardi <dignitysquared@gmail.com>
2021-12-12 22:48:45 -0800

    Implemented game object + basic word state
    Created a new <Game> component and moved everything inside it. The <Game> component will handle all the state junk like words and score and what-have-you.
    
    Additionally, the buttons now change color on hover and click, and actually update the current word when they're clicked on!

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Letter from "./components";
+import Game from "./components";
 import "./components.css";
 
 function App() {
@@ -11,18 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <div className="LetterBox">
-          <Letter letter="L" />
-          <Letter letter="O" />
-          <Letter letter="R" />
-          <Letter letter="E" />
-          <Letter letter="M" />
-          <Letter letter="I" />
-          <Letter letter="P" />
-          <Letter letter="S" />
-          <Letter letter="U" />
-          <Letter letter="M" />
-        </div>
+        <Game />
       </header>
     </div>
   );
--- src/components.js
+++ src/components.js
@@ -1,14 +1,58 @@
+import React from "react";
 import "./components.css";
 
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { word: "" };
+    this.addLetter = this.addLetter.bind(this);
+    this.clearWord = this.clearWord.bind(this);
+  }
+
+  addLetter(letter) {
+    this.setState({ word: this.state.word + letter });
+  }
+
+  clearWord() {
+    this.setState({ word: "" });
+  }
+
+  render() {
+    return (
+      <div>
+        <WordBox currentWord={this.state.word} />
+        <div className="LetterBox">
+          <Letter letter="L" handleClick={(letter) => this.addLetter(letter)} />
+          <Letter letter="O" handleClick={(letter) => this.addLetter(letter)} />
+          <Letter letter="R" handleClick={(letter) => this.addLetter(letter)} />
+          <Letter letter="E" handleClick={(letter) => this.addLetter(letter)} />
+          <Letter letter="M" handleClick={(letter) => this.addLetter(letter)} />
+          <Letter letter="I" handleClick={(letter) => this.addLetter(letter)} />
+          <Letter letter="P" handleClick={(letter) => this.addLetter(letter)} />
+          <Letter letter="S" handleClick={(letter) => this.addLetter(letter)} />
+          <Letter letter="U" handleClick={(letter) => this.addLetter(letter)} />
+          <Letter letter="M" handleClick={(letter) => this.addLetter(letter)} />
+        </div>
+        <button onClick={this.clearWord}>Clear Word</button>
+      </div>
+    );
+  }
+}
+
+const WordBox = (props) => {
+  if (props.currentWord === "") {
+    return <p class="WordBox">Current word: None!</p>;
+  } else {
+    return <p class="WordBox">Current word: {props.currentWord}</p>;
+  }
+};
+
 const Letter = (props) => {
-  let handleClick = () => {
-    console.log(props.letter);
-  };
   return (
-    <div className="Letter" onClick={handleClick}>
+    <div className="Letter" onClick={() => props.handleClick(props.letter)}>
       <p>{props.letter}</p>
     </div>
   );
 };
 
-export default Letter;
+export default Game;
